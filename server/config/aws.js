cd16ad2c6ed4c6f203637421a836dce517ba0610
piapip <thovinh9997@gmail.com>
2021-03-06 11:40:18 +0700

    Remove environment dev file.

--- server/config/aws.js
+++
@@ -1,9 +0,0 @@
-const awsAccessKeyId = "ASIA34VGUMDHQXRFJKW5";
-const awsSecretAccessKey = "q7HqqscZGwOtjxHQXVhqGJdu3PLhcD9UKGkJPUHb";
-const awsSessionToken = "FwoGZXIvYXdzEOH//////////wEaDNoRzSNF4dk4sTWDfyLLAYsJOUCt51IUXcMYgR1sKgSenwDVi5a2AI9hpuuSpbFvK8kvEwqfAeUdm8VJHAXED0/PL+10GVwVXvEbEcsRO+Aq4/6cdFXvjZOMscTpRHLgXLLb5BodV8CdA880KUVJbHvhKSeCelBYkDGWkrZq1oaQfiyfp2FgBAZdMI+L4jjeeUTzTGxnNzWG1a+ybkvEcR6AevKItSsRub/gId0NCpCrqWWTDpXYkY0Vxa47kreAKAjPGPYoAAs9xYMGDf5+DD/dozTPO05m7tB4KIa8h4IGMi2yPdm+w23KpCUD1v83c7UX4ZkCvOkwipU9PKvqxVhF5XEa7ACRYFzQlo1zHdo=";
-const awsBucketName = "end-to-end-slu";
-const awsRegion = "us-east-1";
-
-module.exports = {
-    awsAccessKeyId, awsSecretAccessKey, awsSessionToken, awsBucketName, awsRegion
-}
--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//   mongoURI: 'mongodb://localhost:27017/data-collection'
-// }
-
-module.exports = {
-  // mongoURI: 'mongodb+srv://root:root@cluster0.ppiqk.mongodb.net/data-collection?retryWrites=true&w=majority'
-  mongoURI: 'mongodb+srv://piapip:pakka@cluster0.t3o4s.mongodb.net/data-collection?retryWrites=true&w=majority'
-}
