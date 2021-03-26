e54730689efe33a2b2781642fe799abae1b7ad22
piapip <thovinh9997@gmail.com>
2021-03-26 16:14:57 +0700

    Fixing RejectAudioButton.

--- client/src/components/views/Chatroom/Section/Shared/RejectAudioButton.js
+++ client/src/components/views/Chatroom/Section/Shared/RejectAudioButton.js
@@ -2,18 +2,33 @@ import React, { useState } from 'react';
 import { useDispatch } from "react-redux";
 
 import { Popconfirm } from 'antd';
+import Button from '@material-ui/core/Button';
+import Popover from '@material-ui/core/Popover';
+
 import { removeLatestAudio } from '../../../../../_actions/chatroom_actions';
 
 export default function RejectAudioButton(props) {
 
   const [ buttonState, setButtonState ] = useState(false);
   const [ buttonPhase, setButtonPhase ] = useState(0);
+  const [ anchorEl, setAnchorEl ] = useState(null);
+  const [ popoverOpenStatus, setPopoverOpenStatus ] = useState(false);
 
   const userRole = props ? props.userRole : "";
   const roomID = props ? props.roomID : "";
   const socket = props ? props.socket : null;
   const disabled = props ? props.disabled : true;
   const dispatch = useDispatch();
+
+  const openPopover = (event) => {
+    setAnchorEl(event.currentTarget);
+    setPopoverOpenStatus(true);
+  };
+
+  const closePopover = () => {
+    setAnchorEl(null);
+    setPopoverOpenStatus(false);
+  };
 
   const onReject = async () => {
     await setButtonState(true);
@@ -41,6 +56,22 @@ export default function RejectAudioButton(props) {
     //   1 - xoa thanh cong.
   }
 
+  const popoverPrompt = (
+    <>
+      <p>Bạn sẽ xóa audio mà bên kia vừa gửi tới cho bạn, bạn chắc chắn là sẽ xóa không?</p>
+      <div style={{float: "right"}}>
+        <Button variant="contained" color="secondary" onClick={closePopover}>Không xóa</Button>
+        {
+          buttonState ? (
+            <Button variant="contained" style={{marginLeft: "10px", backgroundColor: "#90caf9", color: "#585D5E"}} disabled>Xóa</Button>
+          ) : (
+            <Button variant="contained" style={{marginLeft: "10px", backgroundColor: "#90caf9", color: "#585D5E"}} onClick={onReject}>Xóa</Button>
+          )
+        }
+      </div>
+    </>
+  )
+
   const insertButton = (
     disabled ? (
       <button className="reject-buttons" disabled>Không hiểu audio</button>
@@ -51,19 +82,26 @@ export default function RejectAudioButton(props) {
           buttonPhase === 1 ? "Đang xóa audio..." : "????HOWWWW???"
         }</button>
       ) : (
-        <Popconfirm 
-          title="Bạn sẽ xóa audio mà bên kia vừa gửi tới cho bạn, bạn chắc chắn là sẽ xóa không?"
-          onConfirm={onReject}
-          okText="Xóa"
-          okButtonProps={{
-            disabled: buttonState
-          }}
-          cancelText="Không xóa">
-          <button className="reject-buttons">{
+        <>
+          <button className="reject-buttons" onClick={openPopover}>{
             buttonPhase === 0 ? "Không hiểu audio" :
             buttonPhase === 1 ? "Đang xóa audio..." : "????HOWWWW???"
           }</button>
-        </Popconfirm>
+          <Popover
+            open={popoverOpenStatus}
+            anchorEl={anchorEl}
+            onClose={closePopover}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}>
+            {popoverPrompt}
+          </Popover>
+        </>
       )
     )
   )
