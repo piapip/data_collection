6113584f38cdc721a37c02dc45840c74c5cc719e
piapip <thovinh9997@gmail.com>
2021-03-31 22:36:20 +0700

    Revert changes from User.

--- server/models/User.js
+++ server/models/User.js
@@ -39,24 +39,24 @@ const userSchema = mongoose.Schema({
 })
 
 
-// userSchema.pre('save', function( next ) {
-//     var user = this;
+userSchema.pre('save', function( next ) {
+    var user = this;
     
-//     if(user.isModified('password')){    
-//         // console.log('password changed')
-//         bcrypt.genSalt(saltRounds, function(err, salt){
-//             if(err) return next(err);
+    if(user.isModified('password')){    
+        // console.log('password changed')
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
     
-//             bcrypt.hash(user.password, salt, function(err, hash){
-//                 if(err) return next(err);
-//                 user.password = hash 
-//                 next()
-//             })
-//         })
-//     } else {
-//         next()
-//     }
-// });
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash 
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+});
 
 userSchema.methods.comparePassword = function(plainPassword,cb){
     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
