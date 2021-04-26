51b861c184d7c695b7c7d4c6884b00dc186eb6bc
piapip <thovinh9997@gmail.com>
2021-04-26 13:59:23 +0700

    Update redis initialization and update package.json

--- server/redis-client.js
+++ server/redis-client.js
@@ -1,2 +1,6 @@
 var redis = require('redis');
-module.exports = redis.createClient(process.env.REDIS_PORT || 6379);
+module.exports = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
