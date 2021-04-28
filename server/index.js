c505a1a73eacf4baed48d3e3c85d4921629b3654
piapip <thovinh9997@gmail.com>
2021-04-28 13:24:24 +0700

    Bug: Pathname of undefined.

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 const port = process.env.PORT || 5000;
 
 app.use("*", (req, res, next) => {
-  console.log(`[${req.method}] ${req._parsedOriginalUrl.pathname}`)
+  console.log(`[${req.method}] ${req._parsedOriginalUrl ? req._parsedOriginalUrl.pathname : ""}`)
   next()
 })
 
